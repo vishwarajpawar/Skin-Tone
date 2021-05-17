53ba7857786f8089e1fed445fbf5e5f8a0b2899a
Vishwaraj <vishwarajsp007@gmail.com>
2021-05-17 09:42:24 +0530

    thrid deployment commit

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'skin_tone/static')]
 STATIC_URL = '/static/'
+STATIC_ROOT  = 'skin_tone/assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
